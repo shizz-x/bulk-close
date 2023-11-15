db48764d05431836a0d62ac1f2c53436ddb58007
shizz-x <lb300@bk.ru>
2023-11-15 05:22:56 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,279 @@
+import logo from "./logo.svg";
+import "./App.css";
+import TokenAccountCloser from "./module/Closer";
+import { useEffect, useRef, useState } from "react";
+import { Buffer } from "buffer";
+import base58 from "bs58";
+
+window.Buffer = Buffer;
+function sleep(ms) {
+  return new Promise((res) => setTimeout(res, ms));
+}
+function App() {
+  const input = useRef();
+  const [closer, setCloser] = useState(undefined);
+  const [text, setText] = useState("");
+  const [STATUS, setStatus] = useState("Paused");
+  const [minTokenAccounts, minTokenAccountsSet] = useState(1);
+  const [source, setsource] = useState(
+    "Put any keys to the area for see first PK"
+  );
+  const [burnTkns, setBurnTkns] = useState(true);
+  const handleCheckboxChange = (event) => {
+    if (event.target.checked) {
+      setBurnTkns(true);
+    } else {
+      setBurnTkns(false);
+    }
+  };
+  const handleTextareaChange = (event) => {
+    console.log(BigInt("123123"));
+    setText(event.target.value);
+    let keys = event.target.value.split("\n").filter((line) => line.trim());
+    try {
+      const closer = new TokenAccountCloser({ privateKeys: keys });
+      setCloser(closer);
+      statusSet(`loaded ${keys.length} wallets`);
+    } catch (e) {
+      statusSet(e.message);
+    }
+  };
+  const handleMinchange = (event) => {
+    if (event.target.value >= 1) {
+      minTokenAccountsSet(event.target.value);
+    }
+  };
+  const statusSet = (msg) => {
+    setStatus(msg);
+    console.log(msg);
+  };
+
+  useEffect(() => {
+    try {
+      if (closer.wallets.length < 1) {
+        setsource("Put any keys to the area for see first PK");
+      }
+      setsource(closer.wallets[0].publicKey.toString());
+    } catch {}
+  }, [closer]);
+  const burn = async (
+    closer,
+    walletFrom,
+    tokenAccount,
+    mint,
+    amount,
+    decimals
+  ) => {
+    const burnTrx = closer.createBurnTransaction(
+      walletFrom.publicKey,
+      tokenAccount,
+      closer.strToPubkey(mint),
+      amount,
+      decimals
+    );
+
+    burnTrx.recentBlockhash = (await closer.recentBlockhash()).blockhash;
+    burnTrx.feePayer = walletFrom.publicKey;
+    burnTrx.sign(walletFrom);
+
+    const trxSignature = await closer.sendRaw(burnTrx.serialize());
+    return closer.confirm(trxSignature);
+  };
+  const start = async () => {
+    if (!closer) {
+      statusSet("Put keys to the area");
+      return 1;
+    }
+    if (closer.wallets.length < 1) {
+      statusSet("Put keys to the area");
+    }
+
+    let sourceWallet = closer.wallets[0];
+    let sourceBalance = await closer.getBalance(sourceWallet.publicKey);
+    if (sourceBalance < 100000) {
+      statusSet("Source wallet dont have enough funds");
+      return 1;
+    }
+
+    const walletsWithFunds = await check();
+
+    if (
+      sourceWallet.publicKey.toString() !==
+      walletsWithFunds[0].publicKey.toString()
+    ) {
+      statusSet(`Transfering ${sourceBalance} lamports from source`);
+      await closer.confirm(
+        await closer.transferSol(
+          sourceWallet,
+          walletsWithFunds[0].publicKey,
+          sourceBalance,
+          (
+            await closer.recentBlockhash()
+          ).blockhash
+        )
+      );
+      await sleep(3000);
+    } else {
+      statusSet("Starting from source");
+    }
+    for (let i = 0; i < walletsWithFunds.length; i++) {
+      const accounts = await closer.getParsedTokenAccountsByPK(
+        walletsWithFunds[i].publicKey
+      );
+      const filtredAccounts = closer.filterParsedTokenAccounts(accounts);
+
+      for (let j = 0; j < filtredAccounts.length; j++) {
+        if (
+          filtredAccounts[j].account.data.parsed.info.tokenAmount.uiAmount > 0
+        ) {
+          statusSet(`burning  ${filtredAccounts[j].pubkey.toString()} account`);
+          // BURNING SHIT IF AMOUNT > 0
+          burn(
+            closer,
+            walletsWithFunds[i],
+            filtredAccounts[j].pubkey,
+            filtredAccounts[j].account.data.parsed.info.mint,
+            BigInt(
+              filtredAccounts[j].account.data.parsed.info.tokenAmount.amount
+            ),
+            filtredAccounts[j].account.data.parsed.info.tokenAmount.decimals
+          );
+          await sleep(3000);
+        }
+      }
+
+      const transactions = closer.generateTransactionsForKeyPair(
+        walletsWithFunds[i],
+        filtredAccounts,
+        (await closer.recentBlockhash()).blockhash
+      );
+
+      const serializedTransactions = transactions.map((t) => t.serialize());
+      statusSet(`closing accounts...`);
+      for (var k = 0; k < serializedTransactions.length; k++) {
+        await closer.confirm(
+          await closer.sendRaw(serializedTransactions[k]),
+          "finalized"
+        );
+      }
+      statusSet(`SUCCESFULLY CLOSED ${filtredAccounts.length} ACCOUNT`);
+
+      if (i + 1 === walletsWithFunds.length) {
+        console.log("done!");
+        return;
+      }
+      await sleep(3000);
+      await closer.confirm(
+        await closer.transferSol(
+          sourceWallet,
+          walletsWithFunds[i + 1].publicKey,
+          await closer.getBalance(walletsWithFunds[i].publicKey),
+          (
+            await closer.recentBlockhash()
+          ).blockhash
+        )
+      );
+      sourceWallet = walletsWithFunds[i + 1];
+      await sleep(3000);
+    }
+  };
+
+  const check = async () => {
+    if (!closer) {
+      statusSet("Put keys to the area");
+      return 1;
+    }
+    if (closer.wallets.length < 1) {
+      statusSet("Put keys to the area");
+    }
+    let walletsWithFunds = [];
+    let totalAccs = 0;
+    let totalTok = 0;
+    for (let i = 0; i < closer.wallets.length; i++) {
+      const accounts = await closer.getParsedTokenAccountsByPK(
+        closer.wallets[i].publicKey
+      );
+
+      const filtredAccounts = closer.filterParsedTokenAccounts(accounts);
+
+      if (filtredAccounts.length >= minTokenAccounts) {
+        totalAccs += filtredAccounts.length;
+
+        walletsWithFunds.push(closer.wallets[i]);
+
+        statusSet(
+          `${walletsWithFunds.length} wallets has ${totalAccs} unclosed accounts, all private keys will printed in console`
+        );
+      }
+      await sleep(1000);
+    } //CHECKING FOR TOKEN ACCOUNTS.
+    if (walletsWithFunds.length < 1) {
+      statusSet(
+        "All accounts clear! (check for minimum amount of token accounts)"
+      );
+    } else {
+      walletsWithFunds.map((wallet) =>
+        console.log(base58.encode(wallet.secretKey))
+      );
+    }
+    return walletsWithFunds;
+  };
+
+  return (
+    <header className="App-header">
+      <textarea
+        ref={input}
+        value={text}
+        onChange={handleTextareaChange}
+        name=""
+        id="INPUT"
+        cols="30"
+        rows="10"
+      ></textarea>
+      <div className="controls">
+        <div className="controls_column">
+          <div className="flex">
+            <span>minimum amount of token accounts:</span>
+            <input
+              value={minTokenAccounts}
+              onChange={handleMinchange}
+              type="number"
+            />
+          </div>
+          <div className="flex">
+            <span>burn all tokens:</span>
+            <input
+              defaultChecked
+              onChange={handleCheckboxChange}
+              type="checkbox"
+            />
+          </div>
+          <button onClick={start}>start</button>
+          <button onClick={check}>check for unclosed token accounts</button>
+        </div>
+      </div>
+      <p>
+        <b>{STATUS}</b>
+      </p>
+      <h1>STATUS:</h1>
+
+      <div className="note">
+        <ul style={{ textAlign: "start" }}>
+          <h2>note</h2>
+          <li>use at your own risk</li>
+          <li>
+            this application "reset" bulk wallets with reveal rented solana
+          </li>
+          <li>send around 0.001SOL to first private key ({source})</li>
+          <li>
+            you can check wallets for unclosed accounts, (minimum amount of
+            token accounts works)
+          </li>
+          <li>all data duplicates in dev console</li>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default App;
---
+++ src/module/Closer.js
@@ -0,0 +1,140 @@
+const web3 = require("@solana/web3.js");
+const spl = require("@solana/spl-token");
+const bs58 = require("bs58");
+
+module.exports = class TokenAccountCloser {
+  constructor(props) {
+    this.wallets = props.privateKeys.map((key) => this.keyToAccount(key));
+
+    this.connection = new web3.Connection(
+      "https://palpable-compatible-cloud.solana-mainnet.quiknode.pro/0a2a500aafd1a4a7817d28d9a7855e1030c24b4a/",
+      "confirmed"
+    );
+
+    this.web3 = web3;
+  }
+
+  signAllTrx(KP, trxs) {
+    return trxs.map((trx) => trx.sign(KP));
+  }
+
+  strToPubkey(str) {
+    return new web3.PublicKey(str);
+  }
+  keyToAccount(key) {
+    try {
+      return web3.Keypair.fromSecretKey(bs58.decode(key));
+    } catch (e) {
+      throw new Error("NON VALID PRIVATE KEY  - " + key);
+    }
+  }
+
+  createBurnTransaction(publicKey, account, mint, amount, decimals) {
+    return new web3.Transaction().add(
+      spl.createBurnCheckedInstruction(
+        account,
+        mint,
+        publicKey,
+        amount,
+        decimals,
+        undefined,
+        spl.TOKEN_PROGRAM_ID
+      )
+    );
+  }
+
+  getParsedTokenAccountsByPK(publicKey) {
+    return this.connection.getParsedTokenAccountsByOwner(publicKey, {
+      programId: spl.TOKEN_PROGRAM_ID,
+    });
+  }
+
+  sendRaw(trx) {
+    return this.connection.sendRawTransaction(trx);
+  }
+
+  confirm(trx) {
+    return this.connection.confirmTransaction(trx, "finalized");
+  }
+
+  recentBlockhash() {
+    return this.connection.getLatestBlockhash();
+  }
+
+  filterParsedTokenAccounts(tokenAccounts) {
+    if (tokenAccounts) {
+      if (tokenAccounts.value.length > 0) {
+        return tokenAccounts.value.filter(
+          (acc) => acc.account.data.parsed.info.state !== "frozen"
+        );
+      }
+    }
+    return [];
+  }
+  chunks(array, chunkSize = 10) {
+    let res = [];
+    for (
+      let currentChunk = 0;
+      currentChunk < array.length;
+      currentChunk += chunkSize
+    ) {
+      res.push(array.slice(currentChunk, currentChunk + chunkSize));
+    }
+    return res;
+  }
+  generateTransactionsForKeyPair(wallet, filteredAccounts, recentBlockhash) {
+    const transactions = [];
+
+    this.chunks(filteredAccounts).forEach((chunk) => {
+      // New empty transaction
+      const txn = new web3.Transaction();
+      txn.feePayer = wallet.publicKey;
+      txn.recentBlockhash = recentBlockhash;
+      for (const account of chunk) {
+        // Add a `closeAccount` instruction for every token account in the chunk
+        txn.add(
+          spl.createCloseAccountInstruction(
+            account.pubkey,
+            wallet.publicKey,
+            wallet.publicKey
+          )
+        );
+      }
+
+      transactions.push(txn);
+    });
+    transactions.map((trx) => trx.sign(wallet));
+    return transactions;
+  }
+
+  getBalance(PK) {
+    return this.connection.getBalance(PK);
+  }
+
+  transferSol(sourceWallet, destinationPublicKey, amount, blockhash) {
+    // Create a new transaction
+    const transaction = new web3.Transaction().add(
+      web3.SystemProgram.transfer({
+        fromPubkey: sourceWallet.publicKey,
+        toPubkey: destinationPublicKey,
+        lamports: amount - 5000, // Amount in lamports (1 SOL = 10^9 lamports)
+      })
+    );
+
+    // Sign the transaction
+    transaction.recentBlockhash = blockhash;
+    transaction.feePayer = sourceWallet.publicKey;
+    transaction.sign(sourceWallet);
+    console.log(
+      `Transferred ${amount / web3.LAMPORTS_PER_SOL} SOL from ${
+        sourceWallet.publicKey
+      } to ${destinationPublicKey}`
+    );
+
+    // Send and confirm the transaction
+    return this.connection.sendRawTransaction(transaction.serialize());
+  }
+  returnWallets() {
+    return this.wallets;
+  }
+};
